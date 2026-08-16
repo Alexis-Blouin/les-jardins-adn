450ab96609a4dcb5297654d6e5743cc7fb026677
Alexis-Blouin <116613522+Alexis-Blouin@users.noreply.github.com>
2026-08-16 15:26:32 -0400

    Make footer an actual footer at the bottom of the page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -157,202 +157,230 @@ function AppContent({ isDark, setIsDark }) {
 
   return (
     <Router>
-      <AppBar position="static">
-        <Container maxWidth="xl">
-          <Toolbar disableGutters>
-            <Box
-              component="img"
-              src={logo_adn}
-              alt="Logo"
-              sx={{
-                width: "40px",
-                height: "40px",
-                marginRight: "16px",
-                display: { xs: "none", md: "flex" },
-              }}
-            />
-            <Typography
-              variant="h6"
-              noWrap
-              component={Link}
-              to="/"
-              sx={{
-                mr: 2,
-                display: { xs: "none", md: "flex" },
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
-              }}
-            >
-              Les Jardins ADN
-            </Typography>
+      <Box
+        sx={{
+          minHeight: "100vh",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <AppBar position="static">
+          <Container maxWidth="xl">
+            <Toolbar disableGutters>
+              <Box
+                component="img"
+                src={logo_adn}
+                alt="Logo"
+                sx={{
+                  width: "40px",
+                  height: "40px",
+                  marginRight: "16px",
+                  display: { xs: "none", md: "flex" },
+                }}
+              />
+              <Typography
+                variant="h6"
+                noWrap
+                component={Link}
+                to="/"
+                sx={{
+                  mr: 2,
+                  display: { xs: "none", md: "flex" },
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+              >
+                Les Jardins ADN
+              </Typography>
 
-            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "left",
+              <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleOpenNavMenu}
+                  color="inherit"
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorElNav}
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "left",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "left",
+                  }}
+                  open={Boolean(anchorElNav)}
+                  onClose={handleCloseNavMenu}
+                  sx={{ display: { xs: "block", md: "none" } }}
+                >
+                  {pages.map((page) => (
+                    <MenuItem
+                      key={page.name}
+                      component={Link}
+                      to={page.path}
+                      onClick={handleCloseNavMenu}
+                    >
+                      <Typography sx={{ textAlign: "center" }}>
+                        {page.name}
+                      </Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </Box>
+              <Box
+                component="img"
+                src={logo_adn}
+                alt="Logo"
+                sx={{
+                  width: "40px",
+                  height: "40px",
+                  marginRight: "16px",
+                  display: { xs: "flex", md: "none" },
                 }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "left",
+              />
+              <Typography
+                variant="h5"
+                noWrap
+                component={Link}
+                to="/"
+                sx={{
+                  mr: 2,
+                  display: { xs: "flex", md: "none" },
+                  flexGrow: 1,
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
                 }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{ display: { xs: "block", md: "none" } }}
               >
+                Jardins ADN
+              </Typography>
+              <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
                 {pages.map((page) => (
-                  <MenuItem
+                  <Button
                     key={page.name}
                     component={Link}
                     to={page.path}
                     onClick={handleCloseNavMenu}
+                    sx={{ my: 2, color: "white", display: "block" }}
                   >
-                    <Typography sx={{ textAlign: "center" }}>
-                      {page.name}
-                    </Typography>
-                  </MenuItem>
+                    {page.name}
+                  </Button>
                 ))}
-              </Menu>
-            </Box>
-            <Box
-              component="img"
-              src={logo_adn}
-              alt="Logo"
-              sx={{
-                width: "40px",
-                height: "40px",
-                marginRight: "16px",
-                display: { xs: "flex", md: "none" },
-              }}
-            />
-            <Typography
-              variant="h5"
-              noWrap
-              component={Link}
-              to="/"
-              sx={{
-                mr: 2,
-                display: { xs: "flex", md: "none" },
-                flexGrow: 1,
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
-              }}
-            >
-              Jardins ADN
-            </Typography>
-            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-              {pages.map((page) => (
-                <Button
-                  key={page.name}
-                  component={Link}
-                  to={page.path}
-                  onClick={handleCloseNavMenu}
-                  sx={{ my: 2, color: "white", display: "block" }}
+              </Box>
+              <Box sx={{ flexGrow: 0 }}>
+                <Tooltip title="Open settings">
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar
+                      alt="Remy Sharp"
+                      src="/static/images/avatar/2.jpg"
+                    />
+                  </IconButton>
+                </Tooltip>
+                <Menu
+                  sx={{ mt: "45px" }}
+                  id="menu-appbar"
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={handleCloseUserMenu}
                 >
-                  {page.name}
-                </Button>
-              ))}
-            </Box>
-            <Box sx={{ flexGrow: 0 }}>
-              <Tooltip title="Open settings">
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                </IconButton>
-              </Tooltip>
-              <Menu
-                sx={{ mt: "45px" }}
-                id="menu-appbar"
-                anchorEl={anchorElUser}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
-              >
-                {settings.map((setting) => (
-                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                    <Typography sx={{ textAlign: "center" }}>
-                      {setting}
-                    </Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
-            </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route
-          path="/produits"
-          element={
-            <List products={products} setProducts={setProducts} user={user} />
-          }
-        />
-        <Route
-          path="/ajouter"
-          element={
-            <ProtectedRoute>
-              <Add setProducts={setProducts} />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/compte/connexion" element={<Login />} />
-        <Route
-          path="/compte/deconnexion"
-          element={
-            <ProtectedRoute>
-              <Logout />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/compte/creer-compte"
-          element={
-            <ProtectedRoute>
-              <CreateAccount />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-      <Box>
-        <Stack
-          direction="row"
-          spacing={2}
-          sx={{ mt: 2, justifyContent: "center" }}
+                  {settings.map((setting) => (
+                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                      <Typography sx={{ textAlign: "center" }}>
+                        {setting}
+                      </Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </Box>
+            </Toolbar>
+          </Container>
+        </AppBar>
+        <Box sx={{ flex: 1 }}>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route
+              path="/produits"
+              element={
+                <List
+                  products={products}
+                  setProducts={setProducts}
+                  user={user}
+                />
+              }
+            />
+            <Route
+              path="/ajouter"
+              element={
+                <ProtectedRoute>
+                  <Add setProducts={setProducts} />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/compte/connexion" element={<Login />} />
+            <Route
+              path="/compte/deconnexion"
+              element={
+                <ProtectedRoute>
+                  <Logout />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/compte/creer-compte"
+              element={
+                <ProtectedRoute>
+                  <CreateAccount />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </Box>
+        <Box
+          component="footer"
+          sx={{
+            py: 2,
+            px: 2,
+            mt: 4,
+            borderTop: "1px solid",
+            borderColor: "divider",
+          }}
         >
-          <Typography variant="body1">
-            @2026 Les Jardins ADN. Tous droits réservés.
-          </Typography>
-          <Link component={RouterLink} to="/compte/connexion">
-            <Typography variant="body1">Connexion</Typography>
-          </Link>
-        </Stack>
+          <Stack direction="row" spacing={2} sx={{ justifyContent: "center" }}>
+            <Typography variant="body1">
+              @2026 Les Jardins ADN. Tous droits réservés.
+            </Typography>
+            {user ? (
+              <Link component={RouterLink} to="/compte/deconnexion">
+                <Typography variant="body1">Déconnexion</Typography>
+              </Link>
+            ) : (
+              <Link component={RouterLink} to="/compte/connexion">
+                <Typography variant="body1">Administrateur</Typography>
+              </Link>
+            )}
+          </Stack>
+        </Box>
       </Box>
     </Router>
   );
