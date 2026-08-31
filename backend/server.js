7b0f7a8671322a0ea67472598adbbc50baf39fb2
Alexis-Blouin <116613522+Alexis-Blouin@users.noreply.github.com>
2026-08-31 19:14:19 -0400

    Update cors urls

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,29 @@ const mysql = require("mysql2"); // MySQL2 client for Node.js
 const cors = require("cors"); // For web security
 const cookieParser = require("cookie-parser"); // For parsing cookies
 
+require("dotenv").config();
+
+// Allowed CORS origins
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(",");
+
 // Create an instance of express
 const app = express();
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(cookieParser());
 
 // Create a connection to the MySQL database
-require("dotenv").config();
 const db = require("./db");
 
 app.get("/", (req, res) => {
@@ -29,7 +44,8 @@ app.use("/products", productsRoutes);
 app.use("/accounts", accountsRoutes);
 app.use("/reservations", reservationsRoutes);
 
-const port = process.env.PORT || 8081;
+const port = process.env.LISTENING_PORT || 8081;
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
