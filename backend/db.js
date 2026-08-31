04b51241f57d248e93070ccf65425424dcc1ea6e
Alexis-Blouin <116613522+Alexis-Blouin@users.noreply.github.com>
2026-08-31 19:22:38 -0400

    Quick setup update

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,13 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  port: process.env.MYSQLPORT,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-});
+const db = mysql
+  .createConnection({
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+  })
+  .promise();
 
 module.exports = db;
