259fea966c7fa7814f736132a7a5c46fdaa93ed0
Alexis-Blouin <116613522+Alexis-Blouin@users.noreply.github.com>
2026-09-01 18:56:36 -0400

    Modifying the map layout a bit

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -32,15 +32,17 @@ function Landing() {
         <Typography variant="body1" sx={{ textAlign: "center" }}>
           Nous somme situés au 3370 Rte 138 O, Sept-Îles, QC G4R 4K1
         </Typography>
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1545.5347839566273!2d-66.50047393655494!3d50.24833437677312!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4c91e19b8c0417c5%3A0x5766249919e14e81!2s3370%20Rte%20138%20O%2C%20Sept-%C3%8Eles%2C%20QC%20G4R%204K1!5e1!3m2!1sen!2sca!4v1788011053636!5m2!1sen!2sca"
-          width="600"
-          height="450"
-          style={{ border: 0 }}
-          allowFullScreen=""
-          loading="lazy"
-          referrerPolicy="strict-origin-when-cross-origin"
-        ></iframe>
+        <Box sx={{ maxWidth: "sm" }}>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1545.5347839566273!2d-66.50047393655494!3d50.24833437677312!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4c91e19b8c0417c5%3A0x5766249919e14e81!2s3370%20Rte%20138%20O%2C%20Sept-%C3%8Eles%2C%20QC%20G4R%204K1!5e1!3m2!1sen!2sca!4v1788011053636!5m2!1sen!2sca"
+            width="100%"
+            height="450"
+            style={{ border: 0, borderRadius: 8 }}
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="strict-origin-when-cross-origin"
+          ></iframe>
+        </Box>
       </Stack>
     </Box>
   );
